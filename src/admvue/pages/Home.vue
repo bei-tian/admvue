c1cf7bbfadf926cc321818f5a154284c734f23db
linxiaoji <406611515@qq.com>
2017-10-11 17:40:28 +0800

    菜单使用数据绑定

--- src/admvue/pages/Home.vue
+++ src/admvue/pages/Home.vue
@@ -6,28 +6,10 @@
         </div>
         <div class="nav-container">
             <ul class="nav-left">
-                <li class="active">
+                <li :class="{active:(navCurrent === index)}" v-for="(item,index) in nav" @click="navChange(index)">
                     <a href="#">
-                        <Icon type="folder"></Icon>
-                        <span>UI示例</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <Icon type="locked"></Icon>
-                        <span>页面示例</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <Icon type="clipboard"></Icon>
-                        <span>表格示例</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <Icon type="gear-a"></Icon>
-                        <span>UI示例</span>
+                        <Icon :type="item.icon"></Icon>
+                        <span>{{item.name}}</span>
                     </a>
                 </li>
             </ul>
@@ -50,49 +32,87 @@
 
 
     <nav class="site-menu">
-        <Menu theme="dark" width="220px">
-            <Submenu name="1">
+        <Menu theme="dark" width="220px" @on-select="openPage">
+            <Submenu :name="i" v-for="(item, i) in menu" :key="i">
                 <template slot="title">
-                    <Icon type="ios-paper"></Icon>
-                    内容管理
+                    <Icon :type="item.icon"></Icon>
+                    {{item.name}}
                 </template>
-                <MenuItem name="1-1">文章管理</MenuItem>
-                <MenuItem name="1-2">评论管理</MenuItem>
-                <MenuItem name="1-3">举报管理</MenuItem>
+                <Menu-item :name="i+'-'+j" v-for="(sub,j) in item.sub" :key="j">{{sub.name}}</Menu-item>
             </Submenu>
-            <Submenu name="2">
-                <template slot="title">
-                    <Icon type="ios-people"></Icon>
-                    用户管理
-                </template>
-                <MenuItem name="2-1">新增用户</MenuItem>
-                <MenuItem name="2-2">活跃用户</MenuItem>
-            </Submenu>
-            <Submenu name="3">
-                <template slot="title">
-                    <Icon type="stats-bars"></Icon>
-                    统计分析
-                </template>
-                <MenuGroup title="使用">
-                    <MenuItem name="3-1">新增和启动</MenuItem>
-                    <MenuItem name="3-2">活跃分析</MenuItem>
-                    <MenuItem name="3-3">时段分析</MenuItem>
-                </MenuGroup>
-                <MenuGroup title="留存">
-                    <MenuItem name="3-4">用户留存</MenuItem>
-                    <MenuItem name="3-5">流失用户</MenuItem>
-                </MenuGroup>
-            </Submenu>
+
         </Menu>
     </nav>
 
 
     <nav class="page-tab">
-        <Tabs type="card" closable :animated="false">
-            <TabPane label="标签一">标签一的内容</TabPane>
-            <TabPane label="标签二">标签二的内容</TabPane>
-            <TabPane label="标签三">标签三的内容</TabPane>
+        <Tabs type="card" closable :animated="false" :value="tabCurrent">
+
+            <Tab-pane v-for="(item,index) in pageTab" :key="index" :label="item.name">dd</Tab-pane>
         </Tabs>
     </nav>
 </div>
 </template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        nav:[
+          {icon:'folder', name:'UI示例', menu:[
+            {icon:'folder', name:'内容管理',sub:[
+              {name:'文章管理'},
+              {name:'评论管理'},
+            ]},
+            {icon:'locked', name:'用户管理',sub:[
+              {name:'新增用户'},
+              {name:'活跃用户'},
+            ]},
+          ]},
+
+
+          {icon:'locked', name:'页面示例', menu:[
+            {icon:'folder', name:'内容管理2',sub:[
+              {name:'文章管理2'},
+              {name:'评论管理2'},
+            ]},
+            {icon:'locked', name:'用户管理2',sub:[
+              {name:'新增用户2'},
+              {name:'活跃用户'},
+            ]},
+          ]},
+          {icon:'clipboard', name:'表格示例'},
+          {icon:'gear-a', name:'UI示例'},
+        ],
+        navCurrent:0,
+
+        menu:[],
+        pageTab:[],
+        tabCurrent:0
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+      navChange(index) {
+        this.navCurrent = index
+        this.menu = this.nav[index].menu
+      },
+      openPage(name) {
+        let keys = name.split('-')
+        let page = this.menu[keys[0]].sub[keys[1]]
+        if(this.pageTab.includes(page)) {
+          this.tabCurrent = this.pageTab.indexOf(page)
+        } else {
+          this.pageTab.push(page)
+          this.tabCurrent = this.pageTab.lastIndexOf(page)
+        }
+      }
+    },
+    beforeMount() {
+      this.menu = this.nav[this.navCurrent].menu
+    }
+  }
+</script>
